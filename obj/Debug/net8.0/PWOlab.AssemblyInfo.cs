ec21f6f96f9e96ed7b706736d11faca333b8afd4
rohithgg <khanarohithfi@gmail.com>
2024-09-17 23:13:03 +0530

    main commit

--- obj/Debug/net8.0/PWOlab.AssemblyInfo.cs
+++ obj/Debug/net8.0/PWOlab.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PWOlab")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22621a70167571f2eecd538d221ca766ed089ccf")]
 [assembly: System.Reflection.AssemblyProductAttribute("PWOlab")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PWOlab")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
